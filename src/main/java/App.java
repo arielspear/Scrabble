5d364c8871fe73c265b1adbb14cac1e1f2759dea
Morgan Lutz & Ariel Spear <Guest@epicodus-109.local>
2015-08-11 14:43:53 -0700

    Calculates Score

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,49 @@
-import java.io.Console;
+import java.util.ArrayList;
 import static spark.Spark.*;
-
+import java.util.Arrays;
+import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-
   }
 
   public Integer scrabbleScore(String word) {
-    return 3;
+
+    String[] wordArray = word.split("");
+    Integer score = 0;
+    String onePoint = "aeioulnrst";
+    String twoPoints = "dg";
+    String threePoints = "bcmp";
+    String fourPoints = "fhvwy";
+    String fivePoints = "k";
+    String eightPoints = "jx";
+    String tenPoints = "qz";
+
+
+
+    for (String letter : wordArray ) {
+
+      if (onePoint.contains(letter)) {
+        score = score + 1;
+      } else if (twoPoints.contains(letter)) {
+          score = score + 2;
+        } else if (threePoints.contains(letter)) {
+          score = score + 3;
+        } else if (fourPoints.contains(letter)) {
+          score = score + 4;
+        } else if (fivePoints.contains(letter)) {
+          score = score + 5;
+        } else if (eightPoints.contains(letter)) {
+          score = score + 8;
+        } else if (tenPoints.contains(letter)) {
+          score = score + 10;
+        } else {
+          System.out.println("That's not a scrabble word");
+        }
+
+    }
+
+      return score;
   }
 
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,6 +8,17 @@ public class AppTest {
   public void scrabbleScore_returnsAScrabbleScoreForALetter_1() {
     App testApp = new App();
     Integer score = 1;
+
     assertEquals(score, testApp.scrabbleScore("a"));
+
   }
+
+  @Test
+  public void scrabbleScore_returnsAScrabbleScoreForAWord_8() {
+    App testApp = new App();
+    Integer score = 8;
+
+    assertEquals(score, testApp.scrabbleScore("hand"));
+  }
+
 }
